8d59fe6dd69c03521d98bb336bd0ee8ca0f09ab4
TiokengSamuel <samueledward40@gmail.com>
2021-04-06 21:21:24 +0100

    bubble sort

---
+++ Sort Algorithms/BubbleSort.java
@@ -0,0 +1,29 @@
+import jdk.internal.vm.annotation.ForceInline;
+
+public class BubbleSort {
+    public static void main (String[] args) {
+        int[] elements = {20, 35, -15, -7, 55, 1, 0};
+
+        for(int lastUnsortedIndex = elements.length; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for(int i = 0; i < lastUnsortedIndex; i++) {
+                if (elements[i] == elements[i+1]) {
+                    swap(elements, i, i+1 );
+                }
+            }
+        }
+    }
+
+    public swap(int[] array, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        int temp;
+
+        array[i] = array[temp];
+        array[j] = array[i];
+        array[temp] = array[j];
+
+    }
+}
