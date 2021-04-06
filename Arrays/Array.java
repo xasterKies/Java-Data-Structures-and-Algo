b391d0cf7e3893fb34ab7ea010ed4dab79464f90
TiokengSamuel <samueledward40@gmail.com>
2021-04-06 20:03:35 +0100

    Arrays

---
+++ Arrays/Array.java
@@ -0,0 +1,17 @@
+
+public class Array {
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = -7;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
