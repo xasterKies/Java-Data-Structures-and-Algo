ef87232e9e36a906a7e80041128b1826c64a206f
TiokengSamuel <samueledward40@gmail.com>
2021-04-09 22:00:58 +0100

    selection sort complete

--- Sort Algorithms/SelectionSort.java
+++ Sort Algorithms/SelectionSort.java
@@ -4,9 +4,21 @@ public class SelectionSort {
     public static void main (String[] args) {
         int[] elements = {20, 35, -15, -7, 55, 1, 0};
 
-        for(int lastUnsortedIndex = elements.length; lastUnsortedIndex < 0; lastUnsortedIndex--) {
-            for(i)
-        }
+        for(int lastUnsortedIndex = elements.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            
+            int largest = 0;
+            for(int i = 1; i <= lastUnsortedIndex; i++) {
+                if(elements[i] > elements[largest]) {
+                    largest = i;
+                    
+                }
+
+                }
+                swap(elements, largest, lastUnsortedIndex);
+
+                }
+
+            
 
         for (int i = 0; i < elements.length; i++) {
             System.out.println(elements[i]);
@@ -15,17 +27,15 @@ public class SelectionSort {
 
     
 
-    public swap(int[] array, int i, int j) {
+    public static void swap(int[] array, int i, int j) {
 
         if (i == j) {
             return;
         }
 
-        int temp;
-
-        array[i] = array[temp];
-        array[j] = array[i];
-        array[temp] = array[j];
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
 
     }
 }
