e4f8c95c47c29368849704280c74e357ac604a7d
TiokengSamuel <samueledward40@gmail.com>
2021-04-11 19:10:58 +0100

    insertion sort

--- Sort Algorithms/InsertionSort.java
+++ Sort Algorithms/InsertionSort.java
@@ -2,7 +2,23 @@ public class InsertionSort {
     public static void main (String[] args) {
         int[] elements = {20, 35, -15, -7, 55, 1, 0};
 
-       //Insertion
+       for(int firstUnsortedIndex = 1; firstUnsortedIndex < elements.length; firstUnsortedIndex++) {
+
+            int newElement = elements[firstUnsortedIndex];
+
+            int i;
+            for(i = firstUnsortedIndex; i > 0 && elements[i-1] > newElement; i--) {
+                elements[i] = elements[i - 1];
+            }
+
+            elements[i] = newElement;
+
+       }
+
+       for(int i = 0; i < elements.length; i++) {
+           System.out.println(elements[i]);
+       }
+    }
 
     
 
