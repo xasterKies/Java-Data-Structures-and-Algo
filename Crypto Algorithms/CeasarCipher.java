bd52bae06218816612b94ef3f736a1f0d88a2d15
TiokengSamuel <samueledward40@gmail.com>
2021-04-14 10:34:12 +0100

    ceasar cipher

---
+++ Crypto Algorithms/CeasarCipher.java
@@ -0,0 +1,24 @@
+public class CeasarCipher {
+    public static void main(String[] args) {
+        String text = "MEETMEATTHEPARK";
+        int s = 4;
+
+    }
+
+    public static StringBuffer encrypt(String text, int s) {
+        StringBuffer result = new StringBuffer();
+
+        for(int i = 0; i <= text.length(); i++) {
+            
+            if(Character.toUpperCase(text.charAt(i))) {
+                char ch = (char)(((int)text.charAt(i) + (s - 65) % 26 + 65));
+                result.append(ch);
+            }
+            else{
+                char ch = (char)(((int)text.charAt(i) + (s - 97) % 26 + 97));
+                result.append(ch);
+            }
+        }
+        return result;
+    }
+}
