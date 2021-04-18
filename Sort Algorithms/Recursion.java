7d11f4ebb617040b9dc69a54452b38cfb2ad49d3
TiokengSamuel <samueledward40@gmail.com>
2021-04-18 21:15:59 +0100

    Recursive Function

--- Sort Algorithms/Recursion.java
+++ Sort Algorithms/Recursion.java
@@ -1,20 +1,31 @@
 class Recursion {
     public static void main(String[] args) {
-
+        //Driver Code
+        System.out.println(iterativeRecursion(3));
+        System.out.println(recursiveRecursion(3));
     }
 
     //Iterative Recursion
-    public int iterativeRecursion(int num) {
+    public static int iterativeRecursion(int num) {
 
         if (num == 0) {
             return 1;
         }
 
         int factorial = 1;
-        for(int i = 1; i < num; i++) {
+        for(int i = 1; i <= num; i++) {
             factorial *= i;
         }
 
         return factorial;
+    }
+
+    //Recursive Recursion
+    public static int recursiveRecursion(int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        return num * recursiveRecursion(num -1);
     }
 }
