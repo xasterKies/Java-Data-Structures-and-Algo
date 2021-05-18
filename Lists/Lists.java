33d3962765ab7a064773fbab5a6c465a824ff00c
TiokengSamuel <samueledward40@gmail.com>
2021-05-18 16:55:55 +0100

    initializing list

---
+++ Lists/Lists.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Lists {
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = new ArrayList<>();
+
+        employeeList.add(new Employee("Tiokeng", "Samuel", 0));
+        employeeList.add(new Employee("Guy", "Bostin", 1));
+        employeeList.add(new Employee("Isaac", "Mathias", 2));
+        employeeList.add(new Employee("Charles", "Hen", 3));
+
+        employeeList.forEach(employee -> System.out.println(employee));
+
+        System.out.print(employeeList.get(1));
+
+    }
+    
+}
