a9387ba27c9884c6c3be2f8273d019d5ee13c2a6
TiokengSamuel <samueledward40@gmail.com>
2021-05-19 10:06:04 +0100

    Single Linked list Implemention for an Employee System

---
+++ Lists/Singly_Linked_List/EmployeeLinkedList.java
@@ -0,0 +1,12 @@
+package Singly_Linked_List;
+
+public class EmployeeLinkedList {
+
+    public EmployeeNode head;
+
+    public addToFront(Employee employee) {
+        Employee node = new Employee(employee);
+        
+    }
+    
+}
---
+++ Lists/Singly_Linked_List/EmployeeNode.java
@@ -0,0 +1,27 @@
+package Singly_Linked_List;
+
+
+public class EmployeeNode {
+
+    public Employee employee;
+    public EmployeeNode next;
+
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public EmployeeNode getNext() {
+        return next;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public  void setNext(EmployeeNode next) {
+        this.next = next;
+    }
+
+    
+}
