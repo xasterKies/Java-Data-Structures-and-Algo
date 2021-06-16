fee43fbdb6692ab62aaaf1e1af850916507c0621
Tiokeng Samuel <40921233+xaster-Kies@users.noreply.github.com>
2021-06-16 23:47:57 +0100

    Stacks done

--- Lists/Lists.java
+++ Lists/Lists.java
@@ -11,7 +11,7 @@ public class Lists {
         employeeList.add(new Employee("Guy", "Bostin", 1));
         employeeList.add(new Employee("Isaac", "Mathias", 2));
         employeeList.add(new Employee("Charles", "Hen", 3));
-//
+//stacks
         employeeList.forEach(employee -> System.out.println(employee));
 
         System.out.print(employeeList.get(1));
