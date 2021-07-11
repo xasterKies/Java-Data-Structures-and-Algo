5889573640ce41224c5f792b4df34498bd7a0932
xaster-Kies <samueledward40@gmai.com>
2021-07-11 10:59:06 +0100

    inserting employees

--- Hash Tables/SimpleHashtable/main.java
+++ Hash Tables/SimpleHashtable/main.java
@@ -2,6 +2,11 @@ import Doubled_Linked_List.Main;
 
 public class main {
    public static void main(String[] args) {
-       
+    
+    //Defining the employees
+    Employee tiokengSamuel = new Employee("Tiokeng", "Samuel", 1);
+    Employee quintaNjom = new Employee("Quinta", "Njom", 2);
+    Employee isaacMathias = new Employee("Isaac", "Mathias", 3);
+    Employee henCharles = new Employee("Hen", "Charles", 4);
    } 
 }
