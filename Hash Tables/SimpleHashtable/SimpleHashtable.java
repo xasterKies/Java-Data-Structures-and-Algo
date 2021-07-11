406c77a50f069452ac9f44e04d4078e443594d49
xaster-Kies <samueledward40@gmai.com>
2021-07-11 10:54:44 +0100

    get hashtable Method

--- Hash Tables/SimpleHashtable/SimpleHashtable.java
+++ Hash Tables/SimpleHashtable/SimpleHashtable.java
@@ -14,6 +14,17 @@
 
     public void put(String key, Employee employee) {
         int hashedKey = hashKey(key);
+        if(hashtable(hashedKey) != null) {
+            System.out.println("Sorry, there's already an employee at position" + hashedKey);
+        }
+        else {
+            hashtable[hashedKey] = employee;
+        }
+    }
+
+    public Employee get(String key) {
+        int hashedKey = hashKey(key);
+        return hashtable(hashedKey);
     }
     
     private int hashKey(String key) {
