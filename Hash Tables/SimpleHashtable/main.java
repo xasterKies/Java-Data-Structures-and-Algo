52d6ae626efbc55c61e428565bd025cfa9c37035
xaster-Kies <samueledward40@gmai.com>
2021-07-11 10:35:04 +0100

    hash Restructuring

---
+++ Hash Tables/SimpleHashtable/main.java
@@ -0,0 +1,7 @@
+import Doubled_Linked_List.Main;
+
+public class main {
+   public static void main(String[] args) {
+       
+   } 
+}
---
+++ Hash Tables/SimpleHashtable/simpleHashtable.java
@@ -0,0 +1,10 @@
+ /* Author:  Tiokeng Samuel
+  * Class: arrayHashtable()
+  * Description: Easy implementation of a Hash table in arrays with the use of the Employee.java class
+  * Date: 11th July 2021
+  */
+
+ public class simpleHashtable {
+    
+     
+ }
