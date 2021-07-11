7434721bf5621d4d64815609c8dca8aef26cc377
xaster-Kies <samueledward40@gmai.com>
2021-07-11 10:26:21 +0100

    Descripion

---
+++ Hash Tables/Employee.java
@@ -0,0 +1,50 @@
+public class Employee {
+    public String firstName;
+    public String lastName;
+    public int age;
+    public int id;
+
+    //Editing employee
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee( FirstName: " + firstName + ", LastName: " + lastName + ", Id: " + id + " )" ;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int id() {
+        return id;
+    }
+}
---
+++ Hash Tables/arrayHashtable.java
@@ -0,0 +1,9 @@
+ /* Author:  Tiokeng Samuel
+  * Class: arrayHashtable()
+  * Description: Easy implementation of a Hash table with the use of the Employee.java class
+  * Date: 11th July 2021
+  */
+ public class arrayHashtable {
+ 
+     
+ }
